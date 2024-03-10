5e73e8db199eef13c5ec7b8d0640ade64ba6276c
Panagiotis897 <130182939+Panagiotis897@users.noreply.github.com>
2024-03-10 23:03:28 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Pnagiotis897'
+description 'A Simple Speedometer UI made by Panagiotis897'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/app.js'
+}
+
+client_script "client/main.lua"
